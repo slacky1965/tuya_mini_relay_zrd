c3e4457494d8dfa8a8cfef9ec3133f39d80aa12e
Fedor <lfa1@yandex.ru>
2025-09-09 22:04:03 +0300

    Update app_dev_config.c

--- src/app_dev_config.c
+++ src/app_dev_config.c
@@ -8,6 +8,7 @@ nv_sts_t config_restore() {
     nv_sts_t st = NV_SUCC;
 
     config->status_onoff = OFF;
+    config->switchType = ZCL_SWITCH_TYPE_MOMENTARY; // ZCL_SWITCH_TYPE_TOGGLE;   // ZCL_SWITCH_TYPE_MOMENTARY
 
     return st;
 }
